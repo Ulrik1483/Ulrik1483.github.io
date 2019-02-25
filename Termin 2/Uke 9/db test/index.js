05f48fc93170bd030beb474cd373219f12368901
Ulrik1483 <42532957+Ulrik1483@users.noreply.github.com>
2019-02-25 20:44:54 +0100

    a

--- Termin 2/Uke 9/db test/index.js
+++ Termin 2/Uke 9/db test/index.js
@@ -1,13 +1,19 @@
-//Lenker index med firebase
-let database = firebase.database();
+//Database
+const db = firebase.database();
+const prosjekter = db.ref("Prosjekt/prosjekter")
+
 
 //Legger innhold til main
-function visProsjekter(snapshot) {
-  let proskjekt = snapshot.val();
-  main.innerHTML+= `
+  function visProsjekt(snapshot) {
+    const key = snap(key);
+    const prosjekt = snap.val();
+
+    main.innerHTML +=`
     <article>
-      <h1>${Prosjekt.prosjekter}</h1>
-      <img src="">
+      <h1>${prosjekter.tittel}</h1>
     </article>
-  `;
+    `;
 }
+
+//Event Listener
+prosjekter.on("child_added", visProsjekt)
