8970b69eb692675eba23c0ae57849b6c5a66477d
Ulrik1483 <uinahl@hotmail.com>
2019-03-04 13:09:19 +0100

    Ferdig med prøve

--- Termin 2/Øve 1/AllYourBaseIsBelongsToUs.js
+++ Termin 2/Øve 1/AllYourBaseIsBelongsToUs.js
@@ -1,22 +1,25 @@
+//HTML-elementer
 let main = document.querySelector("main");
-
+let navn = document.getElementById("navn");
+let melding = document.getElementById("melding");
+//Database
 let db = firebase.database();
-let varer = db.ref("øve_1/varer");
-let skjorter = db.ref("øve_1/varer/skjorter");
-
-function visVarer(snapshot){
-  let vare = snap.val();
-
-  main.innerHTML+=`
-    <article>
-      <h1>${vare.modell} ${vare.merke}</h1>
-      <p>${vare.pris}</p>
-      <img src="Bilder/${vare.bilde}">
-    </article>
+let brukere = db.ref("pokechat/brukere");
+//HTML-funksjoner
+function visMeldinger(snapshot){
+  let bruker = snapshot.val();
+  main.innerHTML +=`
+  <article>
+    <p>${bruker.navn}</p>
+    <p>${bruker.melding}</p>
+  </article>
   `;
 }
 
-function visSkjorter(){
-  main.InnerHTML="";
-  skjorter.on("child_added", visVarer);
-}
+
+
+
+
+
+//Event listener
+brukere.on("child_added", visMeldinger);
