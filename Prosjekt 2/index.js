195ff43debe2dc8217c8404310a3bf36dea0e9e6
Ulrik1483 <uinahl@hotmail.com>
2019-03-18 12:27:09 +0100

    Ikke VÆR FULL

---
+++ Prosjekt 2/index.js
@@ -0,0 +1,13 @@
+"use strict"
+//Database-Elementer
+let database = firebase.database();
+
+//HTML-Elementer
+let main = document.querySelector("main");
+
+//HTML-funksjoner
+function visProdukter(snap){
+
+}
+
+//Event-Listeners
--- Termin 2/Øve 1/Øve 2.js
+++ Termin 2/Øve 1/Øve 2.js
@@ -1,26 +1,7 @@
-let main = document.querySelector("main");
-let txt01 = document.getElementById("txt01");
-let txt02 = document.getElementById("txt02");
-let txt03 = document.getElementById("txt03");
-let key = txt01;
-let db = firebase.database();
-let brukere = db.ref("base01");
-function visBrukere(snap){
-  let bruker = snap.val();
-  main.innerHTML +=`
-    <article>
-      <h1>${bruker.egenskap01}</h1>
-      <p>${bruker.egenskap02}</p>
-      <p>${bruker.egenskap03}</p>
-    </article>
+let ans = prompt("Hva er hovedstaden i Portugal");
 
-  `;
+if (ans == "Lisboa") {
+  alert("Det var riktig")
+} else {
+  alert("Du er en jævla idiot")
 }
-
-function leggTilBrukere(){
-  db.push(txt01).child(key);
-  db.push(txt02).child(key);
-  db.push(txt03).child(key);
-}
-
-brukere.on("child_added", visBrukere);
