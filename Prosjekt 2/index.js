7e1992a8395b2f3a699a53b2c11626c8dc2a5872
Ulrik1483 <42532957+Ulrik1483@users.noreply.github.com>
2019-05-08 19:36:22 +0200

    La til bilder og slideshow

--- Prosjekt 2/index.js
+++ Prosjekt 2/index.js
@@ -38,3 +38,44 @@ function visVare(snap) {
 //Event-Listeners
 dyrene.limitToFirst(3).on("child_added", visVare)
 tingene.limitToFirst(3).on("child_added", visVare)
+
+
+//Bildeslide skript
+var i = 0;
+var antallBilder = 9;
+let bilder = ["Bilder/Slide_bilde_0.png", "Bilder/Slide_bilde_1.png", "Bilder/Slide_bilde_2.png", "Bilder/Slide_bilde_3.png", "Bilder/Slide_bilde_4.png", "Bilder/Slide_bilde_5.png", "Bilder/Slide_bilde_6.png", "Bilder/Slide_bilde_7.png", "Bilder/Slide_bilde_8.png"];
+
+function forhåndslastBilder(array) {
+  if (!forhåndslastBilder.list) {
+      forhåndslastBilder.list = [];
+  }
+  var list = forhåndslastBilder.list;
+  for (var i = 0; i < array.length; i++) {
+      var img = new Image();
+      img.onload = function() {
+          var index = list.indexOf(this);
+      }
+      list.push(img);
+      img.src = array[i];
+  }
+}
+
+forhåndslastBilder(bilder);
+
+function runSlides() {
+  let bildeSlide = document.querySelector(".bildeSlide");
+
+  bildeSlide.innerHTML = `
+    <img src="${bilder[i]}"> 
+    `;
+
+  i += 1;
+
+  if (i == antallBilder) {
+    i = 0;
+  }
+
+  setTimeout(runSlides, 4000);
+}
+
+runSlides();
